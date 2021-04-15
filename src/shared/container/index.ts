3c619d6fe61e3107e807ae04da8ca7c85409dab6
iconicsoda <vitorandresaviandev@gmail.com>
2021-04-15 16:11:18 -0300

    Created Createuser useCase

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { CategoriesRepository } from "../../modules/cars/repositories/implementa
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -17,4 +19,11 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+)
+
+// IUser
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 )
